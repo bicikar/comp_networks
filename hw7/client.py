fe49a83feb931e99ae22d5ab017cff5ab09edc4b
bicikar <kirill_kz2001@mail.ru>
2022-05-22 19:50:45 +0300

    task 4

--- hw7/client.py
+++ hw7/client.py
@@ -1,28 +1,28 @@
 import datetime
+import random
+import sys
+import time
 from socket import socket, AF_INET, SOCK_DGRAM, timeout
 
 TIMEOUT = 1
 IP = 'localhost'
-PORT = 49233
+PORT = 49235
 BUFF_SIZE = 1024
 
 if __name__ == '__main__':
+    port = int(sys.argv[1])
     with socket(AF_INET, SOCK_DGRAM) as udp_socket:
+        udp_socket.bind((IP, port))
         udp_socket.connect((IP, PORT))
         udp_socket.settimeout(TIMEOUT)
-        rtts = []
-        for seq_num in range(10):
+        seq_num = 0
+        while True:
+            seq_num += 1
+            time.sleep(0.5)
+            if random.randint(1, 5) == 1:
+                continue
             start = datetime.datetime.now()
-            message = 'Ping seq_number={seq_number} time={time}'
+            message = '{seq_number} {time}'
             msg_to_send = message.format(seq_number=seq_num,
                                          time=start).encode()
             udp_socket.sendall(msg_to_send)
-            try:
-                data = udp_socket.recv(BUFF_SIZE)
-                rtts.append((datetime.datetime.now() - start).microseconds)
-                print(data.decode())
-            except timeout:
-                pass
-        loss = (1 - len(rtts) / 10.0) * 100
-        print(f'10 packets transmitted, {len(rtts)} received, {round(loss, 2)}% pocket_loss')
-        print(f'rtt min-max-avg = {min(rtts)}-{max(rtts)}-{round(sum(rtts)/len(rtts), 2)}')
